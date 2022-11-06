64c08819ce215e27d11dfb32b31ddf9fc03cd01e
clayton14 <claytoneasley77@gmail.com>
2022-11-06 17:00:53 -0500

    update flatpaks

--- to_install/usr/local/clayton-fedora-config/setup-and-update.sh
+++ to_install/usr/local/clayton-fedora-config/setup-and-update.sh
@@ -4,5 +4,8 @@ dnf install --nobest --assumeyes neovim git
 sudo -u clay git config --global user.name 'Clayton E'
 sudo -u clay git config --global user.email 'claytoneasley77@gmail.com'
 
+flatpak update --system
+sudo -u clay flatpak update --user
+
 pkcon update --noninteractive --only-download
 pkcon offline-trigger --noninteractive
