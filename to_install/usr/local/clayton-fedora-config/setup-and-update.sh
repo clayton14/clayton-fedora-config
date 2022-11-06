66cf0a6195ff1f780d99990b91e8fe0e91cdb35c
clayton14 <claytoneasley77@gmail.com>
2022-11-06 16:55:17 -0500

    configure git

--- to_install/usr/local/clayton-fedora-config/setup-and-update.sh
+++ to_install/usr/local/clayton-fedora-config/setup-and-update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 systemctl set-default graphical.target
-dnf install --nobest --assumeyes neovim
-
+dnf install --nobest --assumeyes neovim git
+sudo -u clay git config --global user.name 'Clayton E'
+sudo -u clay git config --global user.email 'claytoneasley77@gmail.com'
 
 pkcon update --noninteractive --only-download
 pkcon offline-trigger --noninteractive
