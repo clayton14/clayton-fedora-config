e1b1392dbe80fc99654bf02f0f972834ebe543cd
clayton14 <claytoneasley77@gmail.com>
2022-11-06 16:44:02 -0500

    enqueue offline updates after setup service

--- to_install/usr/local/clayton-fedora-config/setup-and-update.sh
+++ to_install/usr/local/clayton-fedora-config/setup-and-update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 systemctl set-default graphical.target
-dnf install --assumeyes neovim
+dnf install --nobest --assumeyes neovim
 
 
+pkcon update --noninteractive --only-download
+pkcon offline-trigger --noninteractive
