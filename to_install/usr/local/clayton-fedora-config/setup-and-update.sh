c46827ed8bc68a2c7221590190d5d5afe92b9c93
clayton14 <claytoneasley77@gmail.com>
2022-11-06 21:54:52 -0500

    echo what script is doing in journalctl

--- to_install/usr/local/clayton-fedora-config/setup-and-update.sh
+++ to_install/usr/local/clayton-fedora-config/setup-and-update.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 systemctl set-default graphical.target
+
+echo Install and configure packages
 dnf install --nobest --assumeyes neovim git
 sudo -u clay git config --global user.name 'Clayton E'
 sudo -u clay git config --global user.email 'claytoneasley77@gmail.com'
 
+echo Flatpak updates:
 flatpak update --system
 sudo -u clay flatpak update --user
 
+echo Enqueue pkcon updates:
 pkcon update --noninteractive --only-download
 pkcon offline-trigger --noninteractive
-
 
