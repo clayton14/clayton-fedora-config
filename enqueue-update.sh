0f72f630850dfe411a454b94d1bdac75ec4e4465
clayton14 <claytoneasley77@gmail.com>
2022-12-18 16:25:50 -0500

    Rename project

--- enqueue-update.sh
+++ enqueue-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd to_install
 sudo cp -r * /
-sudo chmod u=rx,g=,o= /usr/local/clayton-fedora-config/setup-and-update.sh
+sudo chmod u=rx,g=,o= /usr/local/clayton-linux-config/setup-and-update.sh
 sudo systemctl set-default setup-and-update.target
 qdbus org.kde.Shutdown /Shutdown logoutAndReboot
